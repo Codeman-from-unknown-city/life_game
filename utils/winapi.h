04e54a8a99c7a127194da1fddc57201b3443803e
Sergei <i_truschev@mail.com>
2020-12-11 16:03:15 +0900

    add winapi structures

---
+++ utils/winapi.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+
+#define uint8  BYTE
+#define uint16 WORD
+#define uint32 DWORD
+#define	int32  LONG
+
+typedef struct tagBITMAPFILEHEADER {
+  WORD  bfType;
+  DWORD bfSize;
+  WORD  bfReserved1;
+  WORD  bfReserved2;
+  DWORD bfOffBits;
+} BITMAPFILEHEADER, *LPBITMAPFILEHEADER, *PBITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER {
+  DWORD biSize;
+  LONG  biWidth;
+  LONG  biHeight;
+  WORD  biPlanes;
+  WORD  biBitCount;
+  DWORD biCompression;
+  DWORD biSizeImage;
+  LONG  biXPelsPerMeter;
+  LONG  biYPelsPerMeter;
+  DWORD biClrUsed;
+  DWORD biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+
